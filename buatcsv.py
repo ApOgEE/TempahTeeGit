9997f3fbd0ef7332ce5a3d7e62f9546b10db5add
M. Fauzilkamil <apogee@apogeek.com>
2015-06-25 10:54:29 +0800

    ejas skrip nak kira total baju... hehehe

--- buatcsv.py
+++ buatcsv.py
@@ -1,4 +1,14 @@
 #!/usr/local/bin/python
+#import csv
 
-lines = open('Senarai_Order.txt').readlines()
-open('Senarai.csv', 'w').writelines(lines[8:-1])
+lines=open('Senarai_Order.txt').readlines()[8:-1]
+
+total=0
+
+for row in lines:
+   rsplit=row.split(',')
+   total=total+int(rsplit[2])
+
+print 'Total:',total
+
+open('Senarai.csv', 'w').writelines(lines)
